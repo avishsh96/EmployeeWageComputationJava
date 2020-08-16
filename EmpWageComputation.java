4fb14e63294906361834c0f5a847cad8849794d0
Avinash <sharma.avinash96@gmail.com>
2020-08-16 13:29:38 +0530

    added part Time employee and part time wage

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,17 +1,25 @@
 public class EmpWageComputation{
 	public static final int IS_FULL_TIME=1;
-	public static final int EMP_WAGE_PER_HOUR=20;
-
+    	public static final int IS_PART_TIME=2;
+    	public static final int EMP_WAGE_PER_HOUR=20;
+	
 	public static void main(String[] args){
-		System.out.println("Welcome to Employee Wage Computation on master branch");
-		int empHrs;
-        	int randomCheck= (int) (Math.random()*10)%2;
-        	System.out.println(randomCheck);
-        	if (randomCheck==IS_FULL_TIME) {
-            	System.out.println("Present");
-            	empHrs = 8;
-            	System.out.println("Employee Wage: "+empHrs*EMP_WAGE_PER_HOUR);
-        	}
-        	else System.out.println("Absent");
+	int empHrs;
+        int empWage;
+            int randomCheck = (int) (Math.random() * 10) % 3;
+            System.out.println(randomCheck);
+            if (randomCheck == IS_FULL_TIME) {
+                System.out.println("FullTime");
+                empHrs = 8;
+            } else if (randomCheck == IS_PART_TIME) {
+                System.out.println("PartTime");
+                empHrs = 4;
+            }else {
+                System.out.println("Absent");
+                empHrs=0;
+            }
+            empWage = empHrs * EMP_WAGE_PER_HOUR;
+            System.out.println("Employee Wage: " + empWage);
+
 	}
 }
