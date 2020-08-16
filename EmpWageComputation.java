56ed457eaa51dad382c0b3b380611c13c94eb3f1
Avinash <sharma.avinash96@gmail.com>
2020-08-16 13:12:03 +0530

    added daily employee wage

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,9 +1,17 @@
 public class EmpWageComputation{
+	public static final int IS_FULL_TIME=1;
+	public static final int EMP_WAGE_PER_HOUR=20;
+
 	public static void main(String[] args){
 		System.out.println("Welcome to Employee Wage Computation on master branch");
-		int randomCheck= (int) (Math.random()*10)%2;
-		if (randomCheck==1)
-            System.out.println("Employee Present");
-      	else System.out.println("Employee Absent");
+		int empHrs;
+        	int randomCheck= (int) (Math.random()*10)%2;
+        	System.out.println(randomCheck);
+        	if (randomCheck==IS_FULL_TIME) {
+            	System.out.println("Present");
+            	empHrs = 8;
+            	System.out.println("Employee Wage: "+empHrs*EMP_WAGE_PER_HOUR);
+        	}
+        	else System.out.println("Absent");
 	}
 }
