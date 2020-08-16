f2be25e73ffe23841bf5edeae201b81703014d91
Avinash <sharma.avinash96@gmail.com>
2020-08-16 09:39:52 +0530

    added random function to check Emp Present/Absent

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,5 +1,9 @@
 public class EmpWageComputation{
 	public static void main(String[] args){
 		System.out.println("Welcome to Employee Wage Computation on master branch");
+		int randomCheck= (int) (Math.random()*10)%2;
+		if (randomCheck==1)
+            System.out.println("Employee Present");
+      	else System.out.println("Employee Absent");
 	}
 }
