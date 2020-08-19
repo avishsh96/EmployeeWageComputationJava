56c01ebb1663e0aeb49941264e2a08eb9cc9b325
Avinash <sharma.avinash96@gmail.com>
2020-08-19 20:01:26 +0530

    USED procedural programming claculated wage till a condition reached

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,22 +1,18 @@
+//PROCEDURAL PROGRAMMING
+
 public class EmpWageComputation{
-    	public static final int IS_FULL_TIME=1;
-    	public static final int IS_PART_TIME=2;
-    	public static final int EMP_WAGE_PER_HOUR=20;
-    	public static final int NUMBER_WORKING_DAYS=20;
+    public static final int IS_FULL_TIME=1;
+    public static final int IS_PART_TIME=2;
+    public static final int EMP_WAGE_PER_HOUR=20;
+    public static final int NUMBER_WORKING_DAYS=20;
 
-	public static void main(String[] args){
-		EmpWageComputation emp = new EmpWageComputation();
-		emp.calculateWage();
-
-
-	}
-	public void calculateWage(){
-        	int empHrs;
-        	int empWage;
-        	int totalEmpWage=0;
-        	int totalMaxEmpHrs=100;
-        	int hrs=0;
-        	int totalWorkingDays=0;
+    public static void main(String[] args){
+        int empHrs;
+        int empWage;
+        int totalEmpWage=0;
+        int totalMaxEmpHrs=100;
+        int hrs=0;
+        int totalWorkingDays=0;
 
         while (hrs<totalMaxEmpHrs && totalWorkingDays<NUMBER_WORKING_DAYS){
             totalWorkingDays++;
@@ -41,6 +37,7 @@ public class EmpWageComputation{
             System.out.println("Employee Wage: " + empWage);
         }
         System.out.println("Total Wage: "+totalEmpWage);
-    }
 
+
+    }
 }
